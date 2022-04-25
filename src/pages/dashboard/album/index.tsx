2449d3fdadc4cf1e6eb2628807cb71b28f108ff2
WILL SILVA <59093218+WillDevAC@users.noreply.github.com>
2022-04-25 18:26:08 -0500

    Update index.tsx

--- src/pages/dashboard/album/index.tsx
+++ src/pages/dashboard/album/index.tsx
@@ -10,7 +10,7 @@ import Card from '../../../components/dashboard/card'
 const album: React.FC = () => {
   return (
     <LayoutDashboard>
-      <Card Title="Fotinhas formandos 2021" Date="12/04/2022" Link="/fotinhas/formandos.zip"/>
+      <Card Title="Fotos formandos 2021" Date="12/04/2022" Link="/fotinhas/formandos.zip"/>
       <Card Title="Fotos carnaval 2021" Date="12/04/2022" Link="/fotinhas/carnaval.zip"/>
       <Card Title="Galeria de 2019" Date="12/04/2022" Link="/fotinhas/2019.zip"/>
       <Card Title="Galeria de 2020" Date="12/04/2022" Link="/fotinhas/2020.zip"/>
